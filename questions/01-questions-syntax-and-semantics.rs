41fa33dd82eca00f72337d06adb07baf17e3d0db
foj-dev <foj.dev@hotmail.com>
2025-05-17 21:58:48 -0300

    feat: [Update] 01-questions-syntax-and-semantics.rs

--- questions/01-questions-syntax-and-semantics.rs
+++ questions/01-questions-syntax-and-semantics.rs
@@ -11,18 +11,21 @@
 // Which macro would you use to print text on the same line, whihout adding a newline at the end?  Give an example.
 // print();
 fn main () {
+    //
     print("Print: whihout adding a newline at the end!");
 }
 
 // Which macro would you use to print text and automatically add a newline at the end? Give an example.
 // println!();
 fn main () {
+    //
     println!("Print: add newline at the end!");
 }
 
 // line-break
 // How do you insert an explicit newline within a string that is being printed with println!?
 fn main () {
+    //
     println!("First\nSecond!");
 }
 
@@ -42,46 +45,133 @@ fn main () {
 
 // How would you use the format! macro to create the string "The value is: 42" from the number 42?
 fn main () {
-
+    //
     let number = 42;
     let formated = format!("The value is: {}", number);
     println!("{}", formated);
+
 }
 
 // How can you format the number 7 so that it is displayer as "0007" using format!?
+fn main() {
+    //
+    let number: u8 = 7;
+    println!("{:04}", number);
+    
+    let formated = format!("{:04}", number);
+    println!("{}", formated);    
+
+}
+
 // What is the utily of "{:?}" in the format! macro? Give an example of when you would use it.
+// The "{:?}" specifier in the format! macro is used for debug formatting. It's particularly useful for printing data structures like structs, enums, tuples, and vectors in a way that is easy for developers to inspect. It typically shows the structure of the data along with its values.
+#[derive(Debug)]
+struct Point {
+    x:i32,
+    y:i32,
+}
+
+fn main () {
+    let p = Point { x: 10, y: 20 };
+    println!("The point is: {:?}", p);
+}
+
 // What does the specifier "{:#?}" do differently from "{:?}" when formatting a tuple or struct?
+// The specifier "{:#?}" does the same as "{:?}" but with pretty-printing. When formatting tuples or structs, "{:#?}" will often add newlines and indentation to make the output more readable, especially for complex data structures.
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-Variáveis
+fn main () {
+    let rect = Rectangle { width: 10, height: 20 };
+    println!("Debug format: {:?}", rect);
+    println!(" ");
+    println!("Debug format: {:#?}", rect);
+}
 
-    Como você declara uma variável chamada pontuacao e atribui a ela o valor 100?
-    Escreva uma linha de código que declare uma variável cidade com o valor "Paris" e, em seguida, a imprima na tela.
+// variables
+// How do you declare a variable named score and assign in the value 100?
+fn main () {
 
-Mutabilidade
+    //
+    let score: u8 = 100;
+    println!("Value: {}", score);
+    
+}
 
-    Qual palavra-chave é usada para declarar uma variável que pode ter seu valor alterado após a inicialização?
-    Declare uma variável mutável chamada contador inicializada com 0. Em seguida, escreva o código para alterar o valor de contador para 5 e imprimi-lo.
+// Escreva uma linha de código que declare uma variável cidade com o valor "Paris" e, em seguida, a imprima na tela.
+// Write a line of code that declares a variable city with the value "Paris" and then prints it to the screen.
+fn main () {
 
-Constantes
+    //
+    let city: &str = "Paris";
+    println!("City: {}!", city); // output = City: Paris
+    println!("City: {:?}!", city); // output = City: "Paris"
+    
+}
 
-    Como você declara uma constante chamada VELOCIDADE_MAXIMA com o valor 9000 do tipo i32?
-    É possível alterar o valor de uma constante após sua declaração?
+// mutability
+// Which keyword is used to declare a variable that can have its value changed after initialization?
+// mut
 
-Shadowing (Sombreamento)
+// Declare a mutable variable named counter initialized with 0. Then, write the code to change the value of counter to 5 and print it.
+fn main () {
 
-    O que é "shadowing" em Rust? Explique com um exemplo de código e qual seria a saída.
-    Qual será a saída do seguinte código?
-    Rust
+      //
+      let mut counter: u8 = 0;
+      println!("Counter = {:?}", counter);
+      
+      counter = 5;
+      println!("Counter = {:?}", counter);
+
+}
+
+// constants
+// How do you declare a constant named MAX_SPEED with the value 9000 of type i32?
+fn main () {
+
+    //
+    const MAX_SPEED: i32 = 9000;
+    println!("Max Speed: {:?}", MAX_SPEED);
+
+}
+
+// Is it possible to change the value of a constant after its declaration?
+// no
+ 
+// shadowing
+// What is "shadowing" in Rust? Explain with a code example and what the output would be.
+// Shadowing in Rust is the ability to declare a new variable with the same name as a previous variable within the same scope.
+fn main () {
+
+    //
+    let x: i8 = 0;
+    {
+        let x: f32 = 1.0;
+        println!("{:?}", x);
+    }
+    println!("{:?}", x);
+    
+}
+
+// What will be the output of the following code?
+/* Rust
 
     let x = 5;
     let x = x + 1;
     {
         let x = x * 2;
-        println!("O valor de x interno é: {}", x);
+        println!("The inner value of x is: {}", x);
     }
-    println!("O valor de x externo é: {}", x);
+    println!("The outer value of x is: {}", x);
+*/
+// 12
+// 6
 
-Escopo
+
+// Escopo
 
     Qual será a saída do seguinte código? Explique por quê.
     Rust
@@ -96,43 +186,44 @@ Escopo
 
     O que aconteceria se a linha comentada fosse descomentada e compilada?
 
-Tipos Inteiros (u e i)
 
-    Qual é o intervalo de valores que um tipo u8 pode armazenar?
-    Qual é o intervalo de valores que um tipo i8 pode armazenar?
-    Como você declara uma variável idade_maxima do tipo u16 com o valor 150?
-    Como você pode imprimir os valores mínimo e máximo para o tipo u32 usando as constantes associadas ao tipo?
 
-Tipos de Ponto Flutuante (f)
+// Tipos Inteiros (u e i)
+// Qual é o intervalo de valores que um tipo u8 pode armazenar?
+// Qual é o intervalo de valores que um tipo i8 pode armazenar?
+// Como você declara uma variável idade_maxima do tipo u16 com o valor 150?
+// Como você pode imprimir os valores mínimo e máximo para o tipo u32 usando as constantes associadas ao tipo?
 
-    Declare uma variável chamada preco do tipo f32 com o valor 19.99.
-    Qual dos tipos f32 ou f64 oferece maior precisão?
 
-Tipo char
+// Tipos de Ponto Flutuante (f)
+// Declare uma variável chamada preco do tipo f32 com o valor 19.99.
+// Qual dos tipos f32 ou f64 oferece maior precisão?
 
-    Como você declara uma variável chamada inicial que armazena o caractere 'P'?
-    Um tipo char em Rust pode armazenar apenas caracteres ASCII? Explique.
 
-Tipo bool
+// Tipo char
+// Como você declara uma variável chamada inicial que armazena o caractere 'P'?
+// Um tipo char em Rust pode armazenar apenas caracteres ASCII? Explique.
 
-    Declare uma variável chamada esta_chovendo e atribua a ela o valor true.
-    Quais são os dois únicos valores possíveis para uma variável do tipo bool?
 
-Tuplas
+// Tipo bool
+// Declare uma variável chamada esta_chovendo e atribua a ela o valor true.
+// Quais são os dois únicos valores possíveis para uma variável do tipo bool?
 
-    Como você declara uma tupla chamada produto que armazena o nome de um item (string), sua quantidade (inteiro) e seu preço (ponto flutuante)? Por exemplo: ("Caneta", 10, 1.50).
-    Dado a tupla let coordenadas = (10, 20, 30);, como você desestruturaria essa tupla em três variáveis separadas x, y, e z?
-    Como você acessaria o segundo elemento da tupla let rgb = (255, 0, 128);?
 
-Arrays
+// Tuplas
+// Como você declara uma tupla chamada produto que armazena o nome de um item (string), sua quantidade (inteiro) e seu preço (ponto flutuante)? Por exemplo: ("Caneta", 10, 1.50).
+// Dado a tupla let coordenadas = (10, 20, 30);, como você desestruturaria essa tupla em três variáveis separadas x, y, e z?
+// Como você acessaria o segundo elemento da tupla let rgb = (255, 0, 128);?
 
-    Declare um array chamado notas que pode armazenar 5 números do tipo f32. Inicialize-o com alguns valores.
-    Qual é a principal diferença entre um array e uma tupla em termos de tipos de dados que eles podem armazenar?
-    Como você acessaria o primeiro elemento do array let cores = ["vermelho", "verde", "azul"];?
 
-Operadores Aritméticos e Precedência
+// Arrays
+// Declare um array chamado notas que pode armazenar 5 números do tipo f32. Inicialize-o com alguns valores.
+// Qual é a principal diferença entre um array e uma tupla em termos de tipos de dados que eles podem armazenar?
+// Como você acessaria o primeiro elemento do array let cores = ["vermelho", "verde", "azul"];?
 
-    Qual é o resultado da expressão 10 % 3?
-    Qual é o resultado da expressão 5.0 / 2.0?
-    Qual será o resultado da seguinte expressão em Rust, e por quê? println!("{}", 20 - 5 * 2);
-    Qual será o resultado da seguinte expressão em Rust, e por quê? println!("{}", (20 - 5) * 2);
+
+// Operadores Aritméticos e Precedência
+// Qual é o resultado da expressão 10 % 3?
+// Qual é o resultado da expressão 5.0 / 2.0?
+// Qual será o resultado da seguinte expressão em Rust, e por quê? println!("{}", 20 - 5 * 2);
+// Qual será o resultado da seguinte expressão em Rust, e por quê? println!("{}", (20 - 5) * 2);
