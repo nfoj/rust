c0c811d87d2f50005113bc49aeabf798abfd9a29
nfoj <nfoj_@hotmail.com>
2025-07-23 17:59:17 -0300

    feat: update |  05-strings-and-str - 05-q-strings-and-str.rs

---
+++ book-content/questions/05-q-strings-and-str.rs
@@ -0,0 +1,208 @@
+// Write code that creates an empty String and prints it.
+fn main() {
+    //
+    let empty_string: String = String::new();
+    println!("{}", empty_string);
+}
+
+// Create a String from the string literal "Hello, Rust!".
+fn main() {
+    //
+    let literal_string: String = String::from("Hello, Rust!");
+    println!("{}", literal_string);
+}
+
+// Declare a variable that holds a string slice (&str) with the value "Learning Rust".
+fn main() {
+    //
+    let hold_string: &str = "Learning Rust";
+    println!("{}", hold_string);
+}
+
+// Given a String, write the code to get a &str that represents the entire String.
+fn main() {
+    //
+    let input_string: String = String::from("Learning Rust!");
+    let input_str: &str = &input_string;
+    println!("{}", input_str);
+}
+
+// or
+
+fn main() {
+    //
+    let input_string: String = String::from("Learning Rust!");
+    let input_str: &str = input_string.as_str();
+    println!("{}", input_str);
+}
+
+// Write a code that receives a text in "string" and converts it to "str" and then prints it on the screen.
+use std::io;
+
+fn main() {
+    //
+    println!("Enter text:");
+    let mut input_string: String = String::new();
+    io::stdin()
+        .read_line(&mut input_string)
+        .expect("Data entry error!");
+    let text_string: &str = input_string.as_str();
+    println!("\nText converted to &str: {}", text_string);
+}
+
+// Convert the integer 123 into a String.
+use std::io;
+
+fn main() {
+    //
+    println!("Enter number:");
+    let mut input_number: String = String::new();
+    io::stdin()
+        .read_line(&mut input_number)
+        .expect("Data entry error!");
+
+    //
+    let number: u8 = input_number.trim().parse().expect("Data converting error!");
+    println!("Converted value = {}", number);
+}
+
+// Use the .to_string() method or String::from() to create a new String by concatenating a &str and an existing String.
+use std::io;
+
+fn main() {
+    //
+    println!("Enter text:");
+    let mut input_text: String = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("Data entry error!");
+
+    //
+    let text_01: &str = &input_text.as_str();
+    let text_02: String = String::from("string");
+
+    //
+    println!("\n{}", text_01.to_string() + &text_02);
+}
+
+// Create a mutable String and append a &str to it using the push_str() method.
+fn main() {
+    //
+    let mut mutable_string: String = String::from("Hello, ");
+    mutable_string.push_str("word!");
+    println!("{}", mutable_string);
+}
+
+// Add a single character (char) to the end of a mutable String.
+fn main() {
+    //
+    let mut single_char: String = String::from("Hello, word");
+    let character: char = '!';
+    single_char.push(character);
+    println!("{}", single_char);
+}
+
+// Combine a &str, a String, and a number into a new String using the format! macro.
+fn main() {
+    //
+    let var_str: &str = "1";
+    let var_string: String = String::from("2");
+    let var_u8: u8 = 3;
+
+    //
+    let result = format!("{}{}{}", var_str, var_string, var_u8);
+    println!("{}", result);
+}
+
+// Write code that removes the last character from a String.
+use std::io;
+
+fn main() {
+    //
+    println!("Enter text:");
+    let mut input_character: String = String::new();
+    io::stdin()
+        .read_line(&mut input_character)
+        .expect("Data entry erro!");
+
+    let mut character: String = input_character.trim_end().to_string();
+    character.pop();
+    println!("{}", character);
+}
+
+// or
+
+use std::io;
+
+fn main() {
+    //
+    println!("Enter text:");
+    let mut input_character: String = String::new();
+    io::stdin()
+        .read_line(&mut input_character)
+        .expect("Data entry erro!");
+
+    //
+    let mut count = 0;
+    let mut character: String = String::new();
+
+    for i in input_character.trim_end().chars() {
+        count += 1;
+
+        if count < input_character.len() - 1 {
+            character.push(i);
+        }
+    }
+    println!("{}", character);
+}
+
+// Use the .clear() method to empty a String without deallocating its memory capacity.
+fn main() {
+    //
+    let mut var_string: String = String::with_capacity(50);
+    var_string.push_str("Hello, world!");
+
+    //
+    println!("String = {}", var_string);
+    println!("Capacity = {}", var_string.capacity());
+    println!("Len = {}", var_string.len());
+    println!("");
+
+    //
+    var_string.clear();
+    println!("String = {}", var_string);
+    println!("Capacity = {}", var_string.capacity());
+    println!("Len = {}", var_string.len());
+}
+
+// Create a function that takes a &str and returns a slice containing only the first word. Assume that words are separated by spaces.
+
+// Explain in a comment why it is not possible to index a String with an integer (my_string[i]) to get a character. Then, write code that demonstrates the correct way to get the nth character (and not the nth byte).
+
+// Write a function that accepts a &str and returns its first character as a char.
+
+// Create a function that takes a &str and two byte indices (start and end) and returns an Option<&str>. The function should return Some(slice) if the slice is valid (does not cut a UTF-8 character in the middle) and None otherwise.
+
+// Iterate over the bytes of a &str and print each one.
+
+// Iterate over the characters (char) of a &str (that contains multibyte characters, like "olá, mundo 👋") and print each one.
+
+// Write a function that counts the number of characters (char) in a &str, not the number of bytes.
+
+// Check if a &str contains the substring "Rust".
+
+// Find the starting byte index of the first occurrence of the substring "mundo" in "olá, mundo".
+
+// Check if a &str starts with the prefix "http://".
+
+// Check if a filename in a &str ends with the suffix .rs.
+
+// Find all byte indices of a specific character (e.g., 'a') in a &str.
+
+// Replace all occurrences of the substring "error" with "success" in a &str, creating a new String.
+
+// Replace only the first occurrence of "old" with "new" in a &str, creating a new String.
+
+// Write a function that takes a &mut String and replaces all lowercase 'a' characters with uppercase 'A's, modifying the original String.
+
+// Create a new String by removing all whitespace from a &str.
