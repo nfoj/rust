590da20c6136f9bf4e21d9a7710c74ad7c285321
nfoj <nfoj_@hotmail.com>
2025-08-06 21:22:24 -0300

    feat: update } 06-q-functions-and-method-syntax.rs - questions

--- book-content/questions/06-q-functions-and-method-syntax.rs
+++ book-content/questions/06-q-functions-and-method-syntax.rs
@@ -180,3 +180,81 @@ fn main() {
     let num = read_input("Enter number:");
     func_pos_neg_zer(num);
 }
+
+// Crie um programa usando funcoes para receber o valor de 2 numeros digitados pelo usuario e exibir qual o maior
+use std::io;
+
+fn read_input(prompt: &str) -> i8 {
+    //
+    println!("{}", prompt);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).err();
+
+    match input.trim().parse() {
+        Ok(i) => i,
+        Err(_) => {
+            println!("Invalid number!");
+            read_input(prompt)
+        }
+    }
+}
+
+fn func_larger(x: i8, y: i8) {
+    //
+    if x > y {
+        println!("\nNum A > Num B");
+    } else if x < y {
+        println!("\nNum A < Num B");
+    } else {
+        println!("\nNum A == Num B");
+    }
+}
+
+fn main() {
+    let num_a = read_input("Enter number (a):");
+    let num_b = read_input("Enter number (b):");
+
+    func_larger(num_a, num_b);
+}
+
+// Crie um programa usando funcoes que receba as 3 notas de um aluno e com base nas regras a seguir, mostre se ele passou, ficou de recuperacao ou reprovou.
+use std::io;
+
+fn read_input(prompt: &str) -> f32 {
+    //
+    println!("{}", prompt);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).err();
+
+    match input.trim().parse() {
+        Ok(i) => i,
+        Err(_) => {
+            println!("Invalid number!");
+            read_input(prompt)
+        }
+    }
+}
+
+fn func_avarage(x: f32, y: f32, z: f32) {
+    //
+    let ava: f32 = (x + y + z) / 3.;
+
+    if ava < 5. {
+        println!("\nYou failed!");
+    } else if ava >= 5. && ava < 7. {
+        println!("\nYou are in recovery!");
+    } else {
+        println!("\nYou passed!");
+    }
+}
+
+fn main() {
+    //
+    let num_a = read_input("Enter number (a):");
+    let num_b = read_input("Enter number (b):");
+    let num_c = read_input("Enter number (c):");
+
+    func_avarage(num_a, num_b, num_c);
+}
